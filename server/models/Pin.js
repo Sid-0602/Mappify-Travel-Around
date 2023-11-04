4cfdea22405223b446ed0ce676ceb093039ff7b3
Siddhant J <sidbnjadhav@gmail.com>
2023-11-04 13:01:18 +0530

    Database models final push

--- server/models/Pin.js
+++ server/models/Pin.js
@@ -1,26 +1,40 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
+const PinSchema = new mongoose.Schema({
     username:{
+        type:String,
+        require: true,
+    },
+
+    title:{
+        type:String,
+        require: true,
+        min:3,
+    },
+
+    desc: {
         type: String,
         require: true,
         min: 3,
-        max: 20,
-        unique: true,
     },
 
-    email: {
-        type: String,
+    rating:{
+        type: Number,
         require: true,
-        max: 50,
-        unique: true,
+        min: 0,
+        max: 5,
     },
 
-    password: {
-        type: String,
-        required,
-        min: 6,
+    lat: {
+        type: Number,
+        require: true,
     },
+
+    long: {
+        type: Number,
+        require: true,
+    }
+
 }, {timestamps: true});
 
-module.exports = mongoose.model("User",UserSchema);
+module.exports = mongoose.model("Pin",PinSchema);
