743d3913fa405662370ec27f364cc7fffda8f3e7
Siddhant J <sidbnjadhav@gmail.com>
2023-11-05 23:09:25 +0530

    Code push for Pins plotting on Map

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import "mapbox-gl/dist/mapbox-gl.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ReactMapGL, {Marker, Popup } from 'react-map-gl';
 import * as React from 'react';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import StarRateIcon from '@mui/icons-material/StarRate';
 import "./app.css";
+import axios from 'axios';
 
 
 
 const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 function App() {
+
+  const currentUser = "john";
+  const[pins,setPins] = useState([]);
+  const[currentPlaceId, setCurrentPlaceId] = useState(null);
 
   const [viewState, setViewState] = React.useState({
     latitude: 18.516726,
@@ -19,39 +24,63 @@ function App() {
   });
 
   const [showPopup, setShowPopup] = useState(true);
+
+  useEffect(()=>{
+    const getPins = async()=>{
+      try{
+        const res = await axios.get("/pins");
+        setPins(res.data);
+      }catch(err){
+        console.log(err);
+      }
+    };
+    getPins()
+  }, []);
+
+  const handleMarkerClick=(id)=>{
+    setCurrentPlaceId(id);
+  }
   return (
-      <ReactMapGL
+      <ReactMapGL  
       {...viewState}
       mapboxApiAccessToken = {TOKEN}
       onMove={evt => setViewState(evt.viewState)}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-      <Marker longitude={2.294694} latitude={48.858093} anchor="bottom" >
-        <div style={{ position: "relative" }}>
-          <LocationOnIcon style={{ color: "red", fontSize: viewState.zoom*6 }} />
-        </div>
-      </Marker>
-      {showPopup && (
-      <Popup longitude={2.294694} latitude={48.858093}
-        anchor="left">
-        <div className="card">
-          <label>Place</label>
-          <h4 className="place">Eiffle Tower</h4>
-          <label>Review</label>
-          <p className="desc">Beautiful Tower!</p>
-          <label>Rating</label>
-          <div className="stars">
-            <StarRateIcon />
-            <StarRateIcon />
-            <StarRateIcon />
-            <StarRateIcon />
-            <StarRateIcon />
+
+      {pins.map(p=>(
+      <>
+        <Marker longitude={p.lat} latitude={p.long} offsetLeft={-20} offsetTop={-10} anchor="bottom" >
+            <LocationOnIcon 
+              style={{ color: "red", fontSize: viewState.zoom*8, color: p.username===currentUser ? "red" : "slateblue",cursor:"pointer" }} 
+              onClick={()=>handleMarkerClick(p._id)}
+            />
+        </Marker>
+        
+        {p._id === currentPlaceId && (
+          <Popup longitude={p.lat} latitude={p.long}
+          anchor="left" closeButton={true} closeOnClick={false} onClose={()=>setCurrentPlaceId(null)}> 
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">{p.title}</h4>
+            <label>Review</label>
+            <p className="desc">{p.desc}</p>
+            <label>Rating</label>
+            <div className="stars">
+              <StarRateIcon />
+              <StarRateIcon />
+              <StarRateIcon />
+              <StarRateIcon />
+              <StarRateIcon />
+            </div>
+            <label>Info</label>
+            <span className="username">Created by <b>{p.username}</b></span>
           </div>
-          <label>Info</label>
-          <span className="username">Created by <b>Sid J</b></span>
-          <span className="date">1 hour ago!</span>
-        </div>
-      </Popup>)} 
+        </Popup>
+      )}
+      </>
+      ))}
+      
       </ReactMapGL>
 
     )
