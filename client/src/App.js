1b5ee2d1dbc0d04332e99a031886d3cead871648
Siddhant J <sidbnjadhav@gmail.com>
2023-11-05 13:11:11 +0530

    Made Map more interactive

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,26 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import { useState } from "react";
-import Map from 'react-map-gl';
+import ReactMapGL from 'react-map-gl';
+import * as React from 'react';
 
 const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 function App() {
 
-  const [viewPort,setViewPort] = useState({
-    width:"100vw",
-    height:"100vh",
-    latitude: 28.6448,
-    longitude: 77.216,
-    zoom: 4,
+  const [viewState, setViewState] = React.useState({
+    longitude: -100,
+    latitude: 40,
+    zoom: 12
   });
-  return <div>
-    <Map
-    
-    {...viewPort}
-    mapboxAccessToken = {TOKEN}
-    onViewportChange = {nextViewport=> setViewPort(nextViewport)}
-    mapStyle="mapbox://styles/sid-jadhav/clokzow98007p01o46vlt9vfj"
-    />
-  </div>
+  return (
+      <ReactMapGL
+      {...viewState}
+      mapboxApiAccessToken = {TOKEN}
+      onMove={evt => setViewState(evt.viewState)}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+
+    )
 }
 
 export default App;
