068a407a4dd2f56b3a1920eca623aa57bab8cf33
Siddhant <sidjadhav@gmail.com>
2023-11-23 16:10:05 +0530

    Docker Contanerization complete

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use((err, req, res, next) => {
 const port = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
-    res.json("Welcome to Mappify Server!");
+    res.send("Welcome to Mappify Server!");
 })
 
 mongoose
